edd421e8b4d9fc7110cdbbf6b5d8d865130638cf
Thapson <132282346+ThapsonKNyirenda@users.noreply.github.com>
2024-08-21 21:27:00 +0200

    Editing descriptions migrations

---
+++ database/migrations/2024_08_21_192057_drop_cadre_id_nursing_process_id_taxonomy_level_id_syllabus_from_descriptions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('descriptions', function (Blueprint $table) {
+            $table->dropForeign(['cadre_id']);
+            $table->dropColumn('cadre_id');
+
+            $table->dropForeign(['nursing_process_id']);
+            $table->dropColumn('nursing_process_id');
+
+            $table->dropForeign(['taxonomy_level_id']);
+            $table->dropColumn('taxonomy_level_id');
+
+            $table->dropColumn('syllabus');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('descriptions', function (Blueprint $table) {
+            $table->foreignId('cadre_id')->constrained()->onDelete('cascade');
+            $table->foreignId('nursing_process_id')->constrained()->onDelete('cascade');
+            $table->foreignId('taxonomy_level_id')->constrained()->onDelete('cascade');
+            $table->string('syllabus')->nullable();
+        });
+    }
+};
